0ddff2e3e2276856ccce44c39e1a5d4d2cfbe315
huxinjun <hu12315005@126.com>
2020-07-10 17:01:15 +0800

    viewdsl:adv set data before set adapter

--- fastapi/src/main/java/org/pulp/viewdsl/AdvSegments.kt
+++ fastapi/src/main/java/org/pulp/viewdsl/AdvSegments.kt
@@ -2,6 +2,7 @@ package org.pulp.viewdsl
 
 import android.content.Context
 import android.widget.AdapterView
+import kotlin.math.pow
 
 /**
  * segment info for adapter view
@@ -22,6 +23,10 @@ inline fun AdapterView<*>.templete(crossinline init: LvSegmentSets.() -> Unit) {
     this.adapter = ItemViewAdapter<Any> {
         val set = LvSegmentSets(context)
         set.init()
+        val data = getTag(2.toDouble().pow(30.toDouble()).toInt())
+        @Suppress("UNCHECKED_CAST")
+        if (data is MutableList<*>)
+            set.data = data as MutableList<Any>
         set
     }
 
@@ -32,8 +37,10 @@ inline fun AdapterView<*>.data(init: () -> List<Any>) {
 }
 
 inline fun AdapterView<*>.data(append: Boolean, init: () -> List<Any>) {
-    if (adapter == null)
-        throw RuntimeException("data invoke must after templete{...}")
+    if (adapter == null) {
+        setTag(2.toDouble().pow(30.toDouble()).toInt(), init())
+        return
+    }
     @Suppress("UNCHECKED_CAST")
     with(adapter as ItemViewAdapter<*>) {
         if (!append) set.data.clear()
